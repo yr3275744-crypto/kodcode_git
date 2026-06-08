37985c94cd842eafa10d128eaef2cabc7de03392
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-08 15:13:37 +0300

    add get all, get by id and create line functions

--- week9/sql_ex/db.py
+++ week9/sql_ex/db.py
@@ -3,6 +3,7 @@ import time
 
 time.sleep(2)
 def get_conection():
+    """docstring"""
     return mysql.connector.connect(host="localhost",
                                port=3306,
                                user="root",
@@ -11,6 +12,7 @@ def get_conection():
 
 
 def get_schema() -> list:
+    """docstring"""
     conn = get_conection()
     cursor = conn.cursor()
     cursor.execute("DESCRIBE soldiers")
@@ -19,3 +21,38 @@ def get_schema() -> list:
     conn.close()
     return [{"column": row[0], "type": row[1]} for row in rows]
 
+
+def get_all() -> list:
+    """docstring"""
+    conn = get_conection()
+    cursor = conn.cursor(dictionary = True)
+    cursor.execute("SELECT * FROM soldiers")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+def get_by_id(soldier_id:int) -> dict | None:
+    conn = get_conection()
+    cursor = conn.cursor(dictionary = True)
+    cursor.execute("SELECT * FROM soldiers WHERE id = %s", soldier_id)
+    row = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+    return row
+
+def create_line(name:str, ranky:str, unit:str) -> int:
+    """docstring"""
+    conn = get_conection()
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO soldiers (name, ranky, unit) VALUES (%s, %s, %s)", (name, ranky, unit))
+    conn.commit()
+
+    new_id = cursor.lastrowid
+
+    cursor.close()
+    conn.close()
+    return new_id
