f89c1eb199aca61431e74be0aa4e4ed5f3f33d23
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-22 11:08:32 +0300

    add example 1

---
+++ week11/day2/example1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Example1;
+
+class Example1Manager
+{
+    static void Main()
+    {
+        int[] sectorIds = new int[3];
+        sectorIds[0] = 10;
+        sectorIds[1] = 20;
+        sectorIds[2] = 30;
+        Console.WriteLine(sectorIds.Length);
+       
+        List<double> speeds = new List<double>();
+        speeds.Add(412.5);
+        speeds.Add(95.0);
+        speeds.Remove(95.0);
+        Console.WriteLine(speeds.Count);
+    }
+}
