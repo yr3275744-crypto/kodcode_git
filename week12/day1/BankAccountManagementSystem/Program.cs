ebb309bc9bfa2d29691150ef1f0f796f82e7e92c
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-28 17:45:32 +0300

    add active and deactive, add withdrow

--- week12/day1/BankAccountManagementSystem/Program.cs
+++ week12/day1/BankAccountManagementSystem/Program.cs
@@ -14,8 +14,8 @@ class BankAccount
     private string _ownerName;
     private double _balance;
     private AccountTypes _accountType;
-    private bool _isActive = true;
-    private List<string>? _transactionHistory;
+    private bool _isActive;
+    private List<string> _transactionHistory;
 
     public int AccountNumber
     {
@@ -96,6 +96,8 @@ class BankAccount
         OwnerName = ownerName;
         Balance = balance;
         AccountType = accountType;
+        _isActive = true;
+        _transactionHistory = new List<string>();
         Console.WriteLine("hello from full");
     }
 
@@ -106,12 +108,95 @@ class BankAccount
 
     public override string ToString()
     {
-        return $"Account #{AccountNumber} | Owner: {OwnerName} | Balance: ${Balance} | Type: {AccountType}";
+        return $"Account #{AccountNumber} | Owner: {OwnerName} | Balance: ${Balance} | Type: {AccountType:F2}";
+    }
+
+    public void Deposit(double amount)
+    {
+        if (IsActive == false)
+        {
+            Console.WriteLine("Error: The accound does not active");
+        }
+        else if (amount > 0)
+        {
+            Balance = Balance + amount;
+            string message = $"Depositing ${amount} to account #{AccountNumber}";
+            Console.WriteLine(message);
+            _transactionHistory.Add(message);
+        }
+        else
+        {
+            Console.WriteLine("Error: The amount is less then zero");
+        }
+    }
+
+    public bool Withdraw(double amount)
+    {
+        if (IsActive == false)
+        {
+            Console.WriteLine("Error: The accound does not active");
+            return false;
+        }
+        else if (amount > 0 && Balance >= amount)
+        {
+            Balance = Balance - amount;
+            string message = $"Withdrawing ${amount} from account #{AccountNumber}";
+            Console.WriteLine(message);
+            _transactionHistory.Add(message);
+            return true;
+        }
+        else if (amount < 0)
+        {
+            Console.WriteLine("Error: amount < 0. Cennout withdrow");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Error: amount < balance.");
+            return false;
+        }
+    }
+
+    public void ApplyInterest()
+    {
+        if (AccountType == AccountTypes.Savings.ToString())
+        {
+            Balance = Balance + Balance / 50;
+        }
+        else
+        {
+
+        }
+    }
+
+    public void PrintTransactionHistory()
+    {
+        foreach(string message in _transactionHistory)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
     }
 
     static void Main()
     {
+        List<BankAccount> bankAcounts = new List<BankAccount>();
         BankAccount account1 = new BankAccount(1, "jonn");
+        BankAccount account2 = new BankAccount(2, "bob", 100.555, "business");
         Console.WriteLine(account1.ToString());
+        account1.Deposit(50);
+        Console.WriteLine(account1.ToString());
+        account1.Withdraw(40);
+        Console.WriteLine(account1.ToString());
+        account1.PrintTransactionHistory();
     }
 }
