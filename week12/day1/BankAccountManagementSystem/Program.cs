66ac422c262f7917d4c47d864ba2a893599ad083
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-28 16:11:37 +0300

    add the properties and constractores

---
+++ week12/day1/BankAccountManagementSystem/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+namespace BankAccountManagementSystem;
+
+enum AccountTypes
+{
+    Savings,
+    Checking,
+    Business
+}
+
+class BankAccount
+{
+    private int _accountNumber;
+    private string _ownerName;
+    private double _balance;
+    private AccountTypes _accountType;
+    private bool _isActive = true;
+    private List<string>? _transactionHistory;
+
+    public int AccountNumber
+    {
+        get => _accountNumber;
+    }
+
+    public string OwnerName
+    {
+        get => _ownerName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                _ownerName = "Unknown";
+            }
+            else
+            {
+                _ownerName = value;
+            }
+        }
+    }
+
+
+    public double Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+            {
+                _balance = 0;
+            }
+            else
+            {
+                _balance = value;
+            }
+        }
+    }
+
+
+    public string AccountType
+    {
+        get => _accountType.ToString();
+        set
+        {
+            if (Enum.TryParse<AccountTypes>(value, true, out AccountTypes result))
+            {
+                _accountType = result;
+            }
+            else
+            {
+                _accountType = AccountTypes.Checking;
+            }
+        }
+    }
+
+
+    public bool IsActive
+    {
+        get => _isActive;
+        private set
+        {
+            if (value)
+            {
+                _isActive = true;
+            }
+            else
+            {
+                _isActive = false;
+            }
+        }
+    }
+
+
+    public BankAccount(int accountNumber, string ownerName, double balance, string accountType)
+    {
+        _accountNumber = accountNumber;
+        OwnerName = ownerName;
+        Balance = balance;
+        AccountType = accountType;
+        Console.WriteLine("hello from full");
+    }
+
+    public BankAccount(int accountNumber, string ownerName):this(accountNumber, ownerName, 0.0, "")
+    {
+        Console.WriteLine("hello from short");
+    }
+
+    public override string ToString()
+    {
+        return $"Account #{AccountNumber} | Owner: {OwnerName} | Balance: ${Balance} | Type: {AccountType}";
+    }
+
+    static void Main()
+    {
+        BankAccount account1 = new BankAccount(1, "jonn");
+        Console.WriteLine(account1.ToString());
+    }
+}
