25c9696d5e84ab3202000656dc9cccbfcd6f46fd
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-12 15:03:03 +0300

    add the ex project eith report class

---
+++ week14/day1/longEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+namespace longEx;
+
+class ReportsFileManager
+{
+    private string SourceFilePath { get; set; }
+    private string TargetFilePath { get; set; }
+    public ReportsFileManager(string sourceFilePath, string targetFilePath)
+    {
+        SourceFilePath = sourceFilePath;
+        TargetFilePath = targetFilePath;
+    }
+    public void Manege()
+    {
+        using (StreamReader sr = new StreamReader(SourceFilePath))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+
+            }
+        }
+    }
+}
---
+++ week14/day1/longEx/report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace longEx
+{
+    class Report
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public int Priority { get; set; }
+
+        public Report(int id, string catgory, int priority)
+        {
+            Id = id;
+            Category = catgory;
+            Priority = priority;
+        }
+    }
+}
